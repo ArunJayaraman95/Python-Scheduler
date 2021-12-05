e8e0090287ff3100ff455c4d7b2fa958d67a4ba9
Arun Jayaraman <riparun.aj@gmail.com>
2021-12-04 21:46:29 -0500

    Improved readability and deleted comments

--- handler.py
+++ handler.py
@@ -1,10 +1,11 @@
 class Course:
     def __init__(self, name, start, end, days):
         """Days is a list with values from 1-5 for M-F"""
-        self.name = name
+        self.name = name.ljust(12)
         self.start = start
         self.end = end
         self.days = days
+        self.dayOfWeek = {1:"M", 2:"T", 3: "W", 4:"Th", 5:"F"}
 
     def getName(self):
         return self.name
@@ -19,7 +20,10 @@ class Course:
         return self.days
 
     def __repr__(self):
-        return self.name
+        return f'{self.name}:\t {intToTime(self.start)} - {intToTime(self.end)} \ton {"-".join([self.dayOfWeek[x] for x in self.days])}'
+
+def intToTime(t):
+    return f'{str(t)[:-2] if t < 1200 else str(t-1200)[:-2]}:{str(t)[-2:]}{["AM","PM"][t >= 1200]}'
 
 courses = [
     Course("Stats", 1030, 1120, [1, 3, 5]),
@@ -28,26 +32,17 @@ courses = [
     Course("Numerical", 1600, 1715, [2,4]),
     Course("Game Lab", 1900, 2040, [1,3]),
     Course("AI", 1600, 1715, [1,3]),
-    Course("Automata", 1600, 1715, [1,3]),
+    Course("Automata", 1630, 1720, [1,3]),
     Course("Fake", 1715, 1745, [2,4])
 ]
 
-# print(courses[0].getName())
-# print(courses)
-count = len(courses)
-
-switch = [0 for _ in courses]
-print(switch)
-
-inc = "0" * count
-print(f'{inc=}')
-
-limit = 2**count
+numCourses = len(courses)
+limit = 2**numCourses
+possibleSchedules = []
 
 def conflict(A: Course, B: Course):
     """Checks if there's a conflict between 2 classes. In other words, if 2 classes overlap in times on the same days"""
-    # print(A.getStart(), A.getEnd())
-    # print(B.getStart(), B.getEnd())
+
     if A == B:
         return False
 
@@ -57,42 +52,39 @@ def conflict(A: Course, B: Course):
             conflictDay = True
 
     conflictTimes = (A.getStart() <= B.getStart() <= A.getEnd()) or (B.getStart() <= A.getStart() <= B.getEnd())
-    #print(conflictTimes and conflictDay)
     return conflictTimes and conflictDay
 
-possible = []
-for i in range(limit):
-    t = bin(i)[2:].zfill(count)
-    temp = []
+# Check conflicts
+for inc in range(limit):
+    t = bin(inc)[2:].zfill(numCourses)
+    tempCourses = []
 
     for i,b in enumerate(t):
         if b == '1':
-            temp.append(courses[i])
-    #print(temp)
+            tempCourses.append(courses[i])
     poss = True
-    for x in range(0, len(temp)):
-        for y in range(x, len(temp)):
-            #print(f'Comparing {temp[x]} and {temp[y]}')
-            if conflict(temp[x], temp[y]):
-                #print("YES")
-                print(f'Comparing {temp[x]} and {temp[y]}')
-                print("CONFLICT\n")
+    for x in range(0, len(tempCourses)):
+        for y in range(x, len(tempCourses)):
+            if conflict(tempCourses[x], tempCourses[y]):
                 poss = False
             else:
                 pass
     if poss:
-        print(temp)
+        print(tempCourses)
         print("SUCCESS")
-        possible.append(temp)
+        possibleSchedules.append(tempCourses)
         pass
 
-#print(possible)
-print(conflict(courses[1],courses[2]))
+mostClasses = max([len(schedule) for schedule in possibleSchedules])
 
-fullestCount = max([len(arr) for arr in possible])
+
 print('\n\n\nCourse Combos\n=========\n')
-for p in possible:
-    if len(p) >= fullestCount - 1:
-        if len(p) == fullestCount:
-            print("Optimal")
-        print(p, "\n")
+
+for schedule in possibleSchedules:
+    if len(schedule) >= mostClasses - 1:
+        if len(schedule) == mostClasses:
+            print("Optimal:\n")
+        # print(p, "\n")
+        for course in schedule:
+            print(course)
+        print('===========================')
